a2979c4272af91996eaf2d5a06834460cbc53295
iagor <iagoravide@gmail.com>
2025-10-04 12:13:23 -0300

    Alterações em Registro.hpp

--- include/Registro.hpp
+++ include/Registro.hpp
@@ -3,28 +3,26 @@
 
 #include <iostream>
 #include <string>
-#include <cstring>
-#include <algorithm>
-#include <sstream>
 #include "Buffer.hpp"
 
 using namespace std;
 
-const int TAM_NOME = 25; // tamanho fixo do campo nome
-const int TAM_REGISTRO = 29; // tamanho fixo do registro (nome + idade)
-
-enum Formato { FIXO, DELIMITADO, COMPRIMENTO };
+enum class Formato { 
+    FIXO,
+    DELIMITADO,
+    COMPRIMENTO 
+};
 
 class Registro  {
     public:
         Registro() = default;
-        ~Registro();
+        virtual ~Registro() = default;
 
     public:
         virtual void pack (Buffer& buffer, Formato formato) const = 0;
         virtual void unpack (Buffer& buffer, Formato formato) = 0;
-        string getChave ();
-        int getTamanhoFixo ();
+        virtual string getChave () const = 0;
+        virtual int getTamanhoFixo () const = 0;
 
 };
 
