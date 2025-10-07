358788d59bc739ebb239913a4a1fd9567c9d7ef7
Filipe Santos <filipelopesdossantos85@gmail.com>
2025-10-07 18:31:27 -0300

    Manin finalizando com as funçoes de comapraçoes

--- main.cpp
+++ main.cpp
@@ -10,6 +10,46 @@
 
 using namespace std;
 
+bool comaparaRegistros(const vector<RegistroAluno>& vec01, const vector <RegistroAluno>& vec02){
+    if(vec01.size() != vec02.size()){
+        cerr << "ERRO: Quanitidades de registos incompátiveis" << endl;
+        return false;
+    }
+
+    for(size_t i = 0; i < vec01.size(); i++){
+        string nome01 = vec01[i].nome, nome02 = vec02[i].nome;
+
+        nome01.erase(nome01.find_last_not_of(" \t\n\r\f\v") + 1);
+        nome01.erase(0, nome01.find_first_not_of(" \t\n\r\f\v"));
+        nome02.erase(nome02.find_last_not_of(" \t\n\r\f\v") + 1);
+        nome02.erase(0, nome02.find_first_not_of(" \t\n\r\f\v"));
+
+        if(nome01 != nome02 && vec01[i].matricula != vec02[i].matricula && 
+            vec01[i].curso != vec02[i].curso){
+                cerr << "ERRO DE DADOS: Divergencia no registro " << i << endl;
+                cerr << "Esperado: Nome: " << nome01 << ", Matricula: " << vec01[i].matricula << ", Curso: " << vec01[i].curso << endl;
+                cerr << "Esperado: Nome: " << nome01 << ", Matricula: " << vec02[i].matricula << ", Curso: " << vec02[i].curso << endl;
+                return false;
+        }
+
+    }
+
+    return true;
+}
+
+void testReadFile(const string& nameFile, Formato formato){
+    Arquivo<RegistroAluno> arquivo(nameFile, formato);
+    vector<RegistroAluno> registros = arquivo.lerRegistros();
+
+    cout << "Lendo: " << registros.size() << " registros de " << nameFile << ": " << endl;
+    
+    for(const RegistroAluno& aluno : registros){
+        cout << aluno.matricula << " | " << aluno.nome << " | " << aluno.curso << endl;
+    }
+
+    cout << "--------------------------------------------------" << endl;
+}
+
 vector<RegistroAluno> readCSV(const string& path){
     ifstream fileCSV("data/dados.csv");
     vector <RegistroAluno> alunos;
